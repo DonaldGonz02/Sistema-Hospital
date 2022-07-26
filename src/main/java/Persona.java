0ac400f95f3e542e0f3e9305b56aa43afed15483
DonaldGonz02 <rexgrady16@gmail.com>
2022-07-25 21:26:35 -0600

    Clase Persona

---
+++ src/main/java/Persona.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author alumno
+ */
+//crear una super clase Persona (código, nombre, edad, ciudad, contacto)
+public class Persona {
+    
+    int code;
+    String name;
+    int age;
+    String city;
+    String contact;
+    
+    public Persona(int code, String name, int age, String city, String contact){
+        this.name=name;
+        this.age=age;
+        this.city=city;
+        this.contact=contact;
+    }
+    
+}
